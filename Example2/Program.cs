fd8f3fa76045b6fe8282fa7dd7069e428efd8cf0
kadzuki <heaven.roma@mail.ru>
2022-08-09 02:10:47 +0300

    Задача2

---
+++ Example2/Program.cs
@@ -0,0 +1,12 @@
+﻿ //Напишите метод, которая принимает на вход число и выдаёт сумму цифр в числе. 452 -> 11
+
+Console.WriteLine("введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a > 0)
+{
+int num = a % 10;
+a = a / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма: " + sum);
