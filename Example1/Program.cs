1fa8f07f41ca1f04bebb97c982f05de580f3db6c
kadzuki <heaven.roma@mail.ru>
2022-08-09 01:50:35 +0300

    Задача1

---
+++ Example1/Program.cs
@@ -0,0 +1,14 @@
+﻿// int a = 4;
+// int b = 5;
+// Console.WriteLine ("a^b = " + Math.Pow(a, b));
+
+Console.WriteLine("введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+for (int i = 1; i < b; i++)
+{
+stepen = stepen * a;
+}
+Console.WriteLine("a в степени b = " + stepen);
